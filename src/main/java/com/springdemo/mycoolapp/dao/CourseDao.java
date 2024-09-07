4f77353200706a97e16c58a68365cd0a79131077
Mukesh Jha <mukeshjha@Ethan-MacBook-Pro.local>
2024-09-07 23:04:35 +0530

    mysql setup

--- src/main/java/com/springdemo/mycoolapp/dao/CourseDao.java
+++ src/main/java/com/springdemo/mycoolapp/dao/CourseDao.java
@@ -2,6 +2,22 @@ package com.springdemo.mycoolapp.dao;
 
 import com.springdemo.mycoolapp.rest.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseDao extends JpaRepository<Course, Long> {
+
+
+    // Custom JPQL query to find courses by title
+    @Query("SELECT c FROM Course c WHERE c.title = :title")
+    List<Course> findCoursesByTitle(@Param("title") String title);
+
+    // Custom SQL query to find courses by description
+    @Query(value = "SELECT * FROM Course WHERE description = :description", nativeQuery = true)
+    List<Course> findCoursesByDescription(@Param("description") String description);
+
+    // Custom query method to find courses containing a keyword in the title
+    List<Course> findByTitleContaining(String keyword);
 }
