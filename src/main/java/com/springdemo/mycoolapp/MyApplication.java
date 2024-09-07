d70708282420d86b5a8e06ead259f2ae9f4fe150
Mukesh Jha <mukeshjha@Ethan-MacBook-Pro.local>
2024-09-07 23:06:17 +0530

    mysql setup

--- src/main/java/com/springdemo/mycoolapp/MyApplication.java
+++ src/main/java/com/springdemo/mycoolapp/MyApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class MyApplication {
 
     public static void main(String[] args) {
-      ConfigurableApplicationContext context= SpringApplication.run(MyApplication.class, args);
+        ConfigurableApplicationContext context= SpringApplication.run(MyApplication.class, args);
         Alien a =context.getBean(Alien.class);
         a.show();
         System.out.println("Welcome to spring boot");
